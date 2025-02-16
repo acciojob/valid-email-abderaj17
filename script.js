6da5b6815fb32426fa91fd6442fbe7d97723d306
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-16 19:40:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function validEmail(str) {
   //your JS code here.
+	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+// const str = prompt("Enter an email address.");
 alert(validEmail(str));
